23be47b363a15eab707207f18d33bd50cb376727
Andreas Pauley <apauley+github@gmail.com>
2013-02-08 17:12:35 +0200

    Include sponge node in minion startup, ping loop for sponge

--- apps/overlord/src/minion.erl
+++ apps/overlord/src/minion.erl
@@ -8,26 +8,73 @@
          start_link/2,
          init/1]).
 
+-export([ping_loop/1]).
+
 %% These exports are for direct minion commands
 -export([sing/0, sing/1, minion_info/0]).
 
--define(DERANGEDLAUGH_RANDOM_MAX, 15000).
+-define(DERANGEDLAUGH_RANDOM_MAX, 30000).
 
-start_link(HypnoSpongePid, Node) ->
-  Pid = proc_lib:spawn_link(Node, ?MODULE, init, [HypnoSpongePid]),
+
+%% ------------------------------------------------------------------
+%% Startup Functions
+%% ------------------------------------------------------------------
+
+start_link(HypnoSpongePid, RemoteNode) ->
+  Pid = proc_lib:spawn_link(RemoteNode, ?MODULE, init, [{HypnoSpongePid, node()}]),
   {ok, Pid}.
 
-start(HypnoSpongePid, Node) ->
-  Pid = proc_lib:spawn(Node, ?MODULE, init, [HypnoSpongePid]),
+start(HypnoSpongePid, RemoteNode) ->
+  Pid = proc_lib:spawn(RemoteNode, ?MODULE, init, [{HypnoSpongePid, node()}]),
   {ok, Pid}.
 
-init(HypnoSpongePid) ->
+init({HypnoSpongePid, HypnoSpongeNode}) ->
   %% By Overlord decree, in the unlikely event that a master dies, all minions must commit suicide out of respect.
   _Ref = erlang:monitor(process, HypnoSpongePid),
+  _PingPid = proc_lib:spawn(?MODULE, ping_loop, [HypnoSpongeNode]),
 
-  log("~p (~p) Aye, Dark Overlord who lives at ~p~n", [node(), self(), HypnoSpongePid]),
+  log("Aye, Dark Overlord who lives at ~s (~p)~n", [atom_to_list(HypnoSpongeNode), HypnoSpongePid]),
   random:seed(now()),
   minion_wait(HypnoSpongePid).
+
+ping_loop(Node) ->
+  _PingPang = net_adm:ping(Node),
+  timer:sleep(1000),
+  ping_loop(Node).
+
+%% ------------------------------------------------------------------
+%% API Functions
+%% ------------------------------------------------------------------
+
+sing() ->
+  Line1 = "My node now belongs to you",
+  Line2 = "it is yours",
+  Line3 = "I am screwed",
+  Line4 = Line1 ++ " yes you shall cause despair!!!",
+
+  Lines = [Line1, Line2, Line3, Line4],
+  sing(Lines).
+
+sing(Lines) ->
+  display_song(Lines),
+  sing(Lines, os:type()).
+
+sing(Lines, _OSType={unix,darwin}) ->
+  Song = string:join(Lines, " "),
+  Cmd = "osascript -e 'set Volume 6'; say -v cello " ++ Song,
+  os:cmd(Cmd);
+sing(_Lines, _OSType) ->
+  ok.
+
+
+minion_info() ->
+  OTPVersion = erlang:system_info(otp_release),
+  OS = os:type(),
+  {node(), self(), OTPVersion, OS}.
+
+%% ------------------------------------------------------------------
+%% Internal Functions
+%% ------------------------------------------------------------------
 
 minion_wait(HypnoSpongePid) ->
   random_deranged_laugh(),
@@ -52,33 +99,13 @@ minion_message_handler(_Message=sing, HypnoSpongePid) ->
   minion_wait(HypnoSpongePid);
 minion_message_handler(_Message={'DOWN', _Ref, process, DownPid, _Reason}, _HypnoSpongePid) ->
   %% If my parent dies, I too see no reason to live.
-  log("~p ~p My sponge (~p) died of reason '~p' ;-(. I too see no reason to live...~n",
-                                            [node(), self(), DownPid, _Reason]),
+  log("My sponge (~p) died of reason '~p' ;-(. I too see no reason to live...~n",
+      [DownPid, _Reason]),
   erlang:exit(shutdown);
 minion_message_handler(Message, HypnoSpongePid) ->
   log("~p ~p unknown message: ~p~n",[node(), self(), Message]),
   HypnoSpongePid ! {unknown_message, Message, minion_info()},
   minion_wait(HypnoSpongePid).
-
-sing() ->
-  Line1 = "My node now belongs to you",
-  Line2 = "it is yours",
-  Line3 = "I am screwed",
-  Line4 = Line1 ++ " yes you shall cause despair!!!",
-
-  Lines = [Line1, Line2, Line3, Line4],
-  sing(Lines).
-
-sing(Lines) ->
-  display_song(Lines),
-  sing(Lines, os:type()).
-
-sing(Lines, _OSType={unix,darwin}) ->
-  Song = string:join(Lines, " "),
-  Cmd = "osascript -e 'set Volume 6'; say -v cello " ++ Song,
-  os:cmd(Cmd);
-sing(_Lines, _OSType) ->
-  ok.
 
 random_deranged_laugh() ->
   random_deranged_laugh(random:uniform(?DERANGEDLAUGH_RANDOM_MAX)).
@@ -99,11 +126,6 @@ deranged_laugh(_OSType) ->
 
 display_song(Lines) ->
   [erlang:display_string(Line++"\n") || Line <- Lines].
-
-minion_info() ->
-  OTPVersion = erlang:system_info(otp_release),
-  OS = os:type(),
-  {node(), self(), OTPVersion, OS}.
 
 log(String, Params) ->
   darklord_utils:log(?MODULE, String, Params).
