4ef706fb952e7843c9db11b93faa4e4878ad44d5
Andreas Pauley <apauley+github@gmail.com>
2013-02-10 19:10:55 +0200

    Distributed OTP: the evil twin will take over should anything happen to the overlord

--- apps/overlord/src/overlord_app.erl
+++ apps/overlord/src/overlord_app.erl
@@ -9,7 +9,11 @@
 %% Application callbacks
 %% ===================================================================
 
+start({takeover, OtherNode}, _StartArgs) ->
+  darklord_utils:log(?MODULE, "Taking over from ~p~n", [OtherNode]),
+  hypnosponge_sup:start_link();
 start(_StartType, _StartArgs) ->
+  darklord_utils:log(?MODULE, "Starting app: ~p~n", [_StartType]),
   hypnosponge_sup:start_link().
 
 stop(_State) ->
